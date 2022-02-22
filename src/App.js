68fea0fed8c6eb25b6e00755404119437c0c3e0a
Anthea Ip <antheaip964@gmail.com>
2022-02-21 21:12:03 -0500

    Index working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from '@apollo/client';
+const getAll = gql`
+{
+getAll{
+  name 
+  type
+  image
+  }
+}
+`;
+
 
 function App() {
+  const{loading, error, data}=useQuery(getAll)
+if(loading) return "Loading"
+if(error) return "Error"
   return (
     <div className="App">
-      Hello World
+      {
+        data.getAll.map((data) => (
+          <p>
+            {data.name}:{data.type}
+            Image: {data.image}
+          </p>
+        ))};
     </div>
   );
 }
